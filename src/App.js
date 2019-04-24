9f0badd330393453c53596ca329c709d1db24a57
Jonathan Ray <jonathanray17@gmail.com>
2019-04-24 10:32:34 -0400

    clean up event function by putting it in its own function reference

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import Output from './Output';
 
+
+// To share information between children, there has to be a parent to pipe state between the two
+
 class App extends React.Component{
   constructor(props){
     super(props)
@@ -15,11 +18,7 @@ class App extends React.Component{
       <div className="App">
         <header className="App-header">
           <input 
-            onChange={(e) => {
-              this.setState({
-                inputText: e.target.value
-              })
-            }}
+            onChange={this._updateInputText}
           />
           <Output
             text={this.state.inputText}
@@ -27,6 +26,12 @@ class App extends React.Component{
         </header>
       </div>
     );
+  }
+
+  _updateInputText = (e) => {
+    this.setState({
+      inputText: e.target.value
+    })
   }
 }
 
